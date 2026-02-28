5ce84d52ead222d179a0a17933c43d0f985fc32f
__b <serban_tudoralexandru@yahoo.com>
2026-03-01 01:28:29 +0200

    some camera fixes

--- camplayer.gd
+++ camplayer.gd
@@ -1,9 +1,17 @@
 extends Camera2D
 
-@export var speed = 400.0
+@export var speed = 4000.0
 
 var syncPos := Vector2(0.0, 0.0)
 var ui_adjust: Vector2 = Vector2.ZERO
+var add_offset: Vector2 = Vector2.ZERO
+
+@export var decay = 0.8  # How quickly the shaking stops [0, 1].
+@export var max_offset = Vector2(100, 75)  # Maximum hor/ver shake in pixels.
+@export var max_roll = 0.1  # Maximum rotation in radians (use sparingly).
+
+var trauma = 0.0  # Current shake strength.
+var trauma_power = 2  # Trauma exponent. Use [2, 3].
 
 func _enter_tree():
 	# Doing this here instead of on ready prevents bugs.
@@ -15,6 +23,7 @@ func _initialize() -> void:
 		modulate.a = 0.5
 
 func _ready() -> void:
+	randomize()
 	syncPos = global_position
 	$"Player ID".text = name
 	GameManager.player_info[int(name)] = {"spawnpoint":syncPos, "node":self}
@@ -29,6 +38,11 @@ func _physics_process(_delta: float) -> void:
 		position = lerp(position, syncPos, 0.5)
 		return
 	
+	$ui/Label.text = str(is_multiplayer_authority())
+	if trauma:
+		trauma = max(trauma - decay * _delta, 0)
+		shake()
+	
 	var velocity = Vector2.ZERO # The player's movement vector.
 	if Input.is_action_pressed("right_1"):
 		velocity.x += 1
@@ -41,17 +55,18 @@ func _physics_process(_delta: float) -> void:
 	
 	if Input.is_action_just_released("ui_accept"):
 		_showui()
+	if Input.is_key_pressed(KEY_Z):
+		add_trauma(1)
 	
 	if velocity.length() > 0:
-		velocity = velocity.normalized() * speed
+		velocity = velocity.normalized() * speed * _delta
 		$AnimatedSprite2D.play()
 	else:
 		$AnimatedSprite2D.stop()
-
+	
 	syncPos = global_position + offset
-	offset = lerp(offset, offset+velocity, 0.05)
+	offset = lerp(offset, offset+velocity+add_offset, 0.2)
 	$ui.position = offset
-
 
 @rpc("call_local")
 func _showui() -> void:
@@ -63,3 +78,12 @@ func _on_player_disconnected(pid) -> void:
 		GameManager.player_info.erase(pid)
 		queue_free()
 	pass
+
+func add_trauma(amount):
+	trauma = min(trauma + amount, 1.0)
+
+func shake():
+	var amount = pow(trauma, trauma_power)
+	rotation = max_roll * amount * randf_range(-1, 1)
+	add_offset.x = max_offset.x * amount * randf_range(-1, 1)
+	add_offset.y = max_offset.y * amount * randf_range(-1, 1)
--- player.gd
+++ player.gd
@@ -9,19 +9,21 @@ func _enter_tree():
 	# Doing this here instead of on ready prevents bugs.
 	set_multiplayer_authority(int(str(name)))
 
-@rpc("any_peer")
+@rpc("call_local")
 func _initialize() -> void:
-	if is_in_group("shooter"): modulate = Color(0, 0, 0)
+	if is_in_group("shooter"): $AnimatedSprite2D.modulate = Color(0, 0, 0)
 	if is_in_group("assistant"): 
 		if int(str(name)) == get_multiplayer_authority():
 			$ColorRect.show()
-			modulate.a = 0.5
+			_showui()
 
 func _ready() -> void:
 	syncPos = global_position
 	$"Player ID".text = name
 	GameManager.player_info[int(name)] = {"spawnpoint":syncPos, "node":self}
 	NetworkManager.player_disconnected.connect(_on_player_disconnected)
+	#await get_tree().create_timer(get_physics_process_delta_time(), false, true).timeout
+	#Input.action_press("ui_accept")
 
 func _physics_process(_delta: float) -> void:
 	if not is_multiplayer_authority():
@@ -29,6 +31,7 @@ func _physics_process(_delta: float) -> void:
 		position = lerp(position, syncPos, 0.5)
 		return
 	
+	$ui/Label.text = str(is_multiplayer_authority())
 	velocity = Vector2.ZERO # The player's movement vector.
 	if Input.is_action_pressed("right_1"):
 		velocity.x += 1
@@ -38,7 +41,11 @@ func _physics_process(_delta: float) -> void:
 		velocity.y += 1
 	if Input.is_action_pressed("up_1"):
 		velocity.y -= 1
-
+	
+	if Input.is_action_just_pressed("ui_accept"):
+		_showui()
+	$ui.global_position = lerp($ui.global_position, get_tree().get_first_node_in_group("assistant").global_position, 0.2)
+	
 	if velocity.length() > 0:
 		velocity = velocity.normalized() * speed
 		$AnimatedSprite2D.play()
@@ -54,3 +61,7 @@ func _on_player_disconnected(pid) -> void:
 		GameManager.player_info.erase(pid)
 		queue_free()
 	pass
+
+@rpc("call_local")
+func _showui() -> void:
+	$ui.show()
